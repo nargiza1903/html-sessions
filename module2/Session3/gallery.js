d9c1c2324037b73d06d353d984a3d98d99439f52
nargiza1903 <121645300+nargiza1903@users.noreply.github.com>
2024-08-18 16:57:46 -0400

    created new gallery

---
+++ module2/Session3/gallery.js
@@ -0,0 +1,11 @@
+// gallery.js
+document.addEventListener('DOMContentLoaded', () => {
+    const mainImage = document.getElementById('main-image');
+    const thumbnails = document.querySelectorAll('.thumbnail');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', () => {
+            mainImage.src = thumbnail.src;
+        });
+    });
+});
